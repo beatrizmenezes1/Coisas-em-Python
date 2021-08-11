89e2d8fe51de61b25cc7f78e83332ea6a6f83898
Beatriz Pires de Menezes <beatrizmz.26menezes@gmail.com>
2021-08-11 17:49:34 -0300

    Create operacoesmath.py
    operações básicas

---
+++ CodesAleatorios/operacoesmath.py
@@ -0,0 +1,37 @@
+print('-----Desafio-----')
+print('As 4 operaçoes basicas com 2 numeros')
+print('-------------------------------------------------------------')
+
+print('Soma')
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+totalsoma = (n1+n2)
+print('A soma de {} + {} é {}'.format(n1,n2,totalsoma))
+
+print('-------------------------------------------------------------')
+         
+print('Subtracao')
+s1 = int(input('Digite um numero: '))
+s2 = int(input('Digite outro numero: '))
+totalsub = (s1-s2)
+print('A diferença entre {} e {} é {}'.format(s1,s2,totalsub))
+
+print('-------------------------------------------------------------')
+
+print('Multiplicacao')
+m1 = int(input('Digite um numero: '))
+m2 = int(input('Digite outro numero: '))
+totalmult = (m1*m2)
+print('A multiplicacao entre {} e {} é {}'.format(m1,m2,totalmult))
+
+print('-------------------------------------------------------------')
+
+print('Divisao')
+d1 = float(input('Digite um numero: '))
+d2 = float(input('Digite outro numero: '))
+totaldiv = (d1/d2)
+print('A divisao ente {} por {} é {}'.format(d1,d2,totaldiv))
+
+print('--------------------')
+print('Processo Finalizado')
+print('--------------------')
