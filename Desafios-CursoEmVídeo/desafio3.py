75a95d87061d4d3fb4a4e380e7e66d8042179317
Beatriz Pires de Menezes <beatrizmz.26menezes@gmail.com>
2021-08-11 15:05:12 -0300

    Create desafio3.py

---
+++ Desafios-CursoEmVídeo/desafio3.py
@@ -0,0 +1,7 @@
+print('----- Desafio 3 -----')
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+total = (num1+num2)
+
+print('A soma de', num1,'+', num2,'= ',total)
+print('Processo FInalizado')
